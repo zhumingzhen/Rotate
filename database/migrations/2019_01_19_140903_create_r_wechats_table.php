6bcf087ba81ee08b99893bb3e3d347d03383b1f3
vagrant <vagrant@homestead>
2019-01-19 14:21:09 +0000

    Wechat tabble

---
+++ database/migrations/2019_01_19_140903_create_r_wechats_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRWechatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('r_wechats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('subscribe');
+            $table->string('openid');
+            $table->string('nickname');
+            $table->tinyInteger('sex');
+            $table->string('city');
+            $table->string('country');
+            $table->string('province');
+            $table->string('headimgurl');
+            $table->string('subscribe_time');
+            $table->string('subscribe_end_time');
+            $table->string('remark');
+            $table->string('subscribe_scene');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('r_wechats');
+    }
+}
