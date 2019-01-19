9cb4b33255747245f0a98e4adebfa11872195e75
vagrant <vagrant@homestead>
2019-01-19 15:15:20 +0000

    fillable

--- app/RWechat.php
+++ app/RWechat.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class RWechat extends Model
 {
     protected $fillable = [
-        'subscribe','openid','nickname','sex','city','province','country','headimgurl','subscribe_time','subscribe_scene'
+        'subscribe','openid','nickname','sex','city','province','country','headimgurl','subscribe_time','subscribe_scene', 'subscribe_end_time'
     ];
 }
